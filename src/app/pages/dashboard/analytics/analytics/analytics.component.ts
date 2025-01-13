d08fcc051cb63275445c8b98324977c184530735
U-Alice <umugwanezaalice22@gmail.com>
2025-01-13 10:47:58 +0200

    [feat]: Prime Ng Toast and messages

--- src/app/pages/dashboard/analytics/analytics/analytics.component.ts
+++ src/app/pages/dashboard/analytics/analytics/analytics.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { ChartModule } from 'primeng/chart';
+
 @Component({
   selector: 'app-analytics.component',
-  imports: [ChartModule, BrowserModule],
+  imports: [ChartModule],
   templateUrl: './analytics.component.html',
   styleUrl: './analytics.component.css'
 })
+
 export class AnalyticsComponentComponent {
   chartData = [
     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
